a4d872422a27488698e66ceec12be18d2539b771
RoyalGoose <velesn@ya.ru>
2020-12-21 13:41:35 +0300

    deleting and log

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 from flask_cors import CORS
-import json
 from pymongo import MongoClient
+from datetime import datetime
 
 MongoUrl = 'mongodb://YouJin:YouJinPwd@82.148.31.138:27017/ps?authSource=YouJin'
 MongoDb = 'YouJin'
@@ -12,6 +12,14 @@ coll = db[MongoColl]
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+def slog(type='info', txt=''):
+    txt = type + datetime.now().strftime(" %d.%m.%Y %H:%M:%S.%f ") + str(txt) + '\n'
+    # print(txt.replace('\n', ''))
+    f = open('server.log', 'a', encoding='utf-8')
+    f.write(txt)
+    f.close()
 
 
 def dbread(key):
@@ -25,7 +33,7 @@ def dbread(key):
             elements.append(element)
         return elements
     except Exception as ex:
-        print(ex)
+        slog('exep', '| dbread | %s' % ex)
         return 'Failed'
 
 
@@ -35,23 +43,39 @@ def dbwrite(data):
         coll.insert_many([data])
         return True
     except Exception as ex:
-        print(ex)
+        slog('exep', '| dbwrite | %s' % ex)
+        return False
+
+
+def dbdelete(data):
+    try:
+        coll.remove({'id': data})
+        return True
+    except Exception as ex:
+        slog('exep', '| dbdelete | %s' % ex)
         return False
 
 
 @app.route('/', methods=['POST'])
 def mainpost():
+    slog('info', '| post | Request from %s' % request.remote_addr)
     req = request.args.to_dict()
-    if dbwrite(req):
-        return 'Success'
+    if 'action' in req.keys() and req.get('action') == 'delete':
+        if dbdelete(req['id']):
+            return 'Success'
+        else:
+            return 'Failed'
     else:
-        return 'Failed'
+        if dbwrite(req):
+            return 'Success'
+        else:
+            return 'Failed'
 
 
 @app.route('/', methods=['GET'])
 def main():
+    slog('info', '| main | Request from %s' % request.remote_addr)
     req = request.args.to_dict()
-    print(req)
     try:
         if 'id' in req.keys():
             data = dbread(req['id'])
@@ -65,9 +89,9 @@ def main():
             result[ID] = element
         return result
     except Exception as ex:
-        print(ex)
+        slog('exep', '| main | %s' % ex)
         return 'Failed'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=False)
